4b5baa6058b3ebde10edcf2aa3ba14af5788406c
lingnili <wmdlifecoba@gmail.com>
2019-11-12 22:40:51 +0800

    add Mongoose connection and create Todo model

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose"); //載入mongoose
+
+//設定連線到model中的todo資料庫
+mongoose.connect("mongodb://127.0.0.1/todo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//成功連線後mongoose會回傳一個connection
+const db = mongoose.connection;
+
+//連線異常時 使用on來監聽，只要有觸發error就會執行
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+//連線成功 使用once來監聽，只會觸發一次
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+//載入todo model
+const Todo = require("./models/todo");
 
 app.get("/", (req, res) => {
   res.send("this will be a todo app");
---
+++ models/todo.js
@@ -0,0 +1,15 @@
+//設定todo資料庫儲存內容
+const mongoose = require("mongoose");
+//Mongoose 是一個 schema-based 的 ODM
+const Schema = mongoose.Schema;
+
+const todoschema = new Schema({
+  name: {
+    type: String, //name型態
+    required: true //是否必須
+  }
+  //todo model中每筆資料型態為object，內容含name
+});
+
+//將todo輸出為Todo(資料庫慣例為大寫開頭)
+module.exports = mongoose.model("Todo", todoschema);
