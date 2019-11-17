dcfef6ccb85ff2f934aac90c788c7af00130ae2a
lingnili <wmdlifecoba@gmail.com>
2019-11-17 13:24:55 +0800

    Add Bootstrap

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ const Todo = require("../models/todo");
 //設定route
 router.get("/", (req, res) => {
   Todo.find({}) //找到所有資料
-    .sort({ name: "asc" }) //按照asc方法排序 sort為mongoose提供的method
+    .sort({ name: "asc", test: 1 }) //按照asc方法排序 sort為mongoose提供的method
     .exec((err, todos) => {
       if (err) return console.log(err);
       return res.render("index", { todos });
