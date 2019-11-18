8721b4f863a3c19629138007f75dfeb6de9fe6cd
lingnili <wmdlifecoba@gmail.com>
2019-11-18 19:59:30 +0800

    Add User model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  date: {
+    type: Date, //儲存資料的type為Date
+    default: Date.new //預設為建立帳號當下的時間戳記
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
 
 //log in page and action
 router.get("/login", (req, res) => {
